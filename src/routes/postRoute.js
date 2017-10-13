c64f155c5e3b01b919bbfd1b1f12c6578a2b14a7
Scott <mymislife@gmail.com>
2017-10-13 22:52:50 +0800

    requireAuth for post routes

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const {empty_error} = require("../consts/Exception");
 const {Post} = require("../models");
+const requireAuth = require("../passports/requireAuth");
 const adminService = require("../services/adminService");
 
 const router = express.Router();
 
 const model = Post;
 
-router.get('/admin/post', (req, res, next) => {
+router.get('/admin/post', requireAuth, (req, res, next) => {
     let {query={}} = req;
     const projection = { _id: 1, title: 1, body: 1 };
     const sort = {priority: -1};
@@ -16,28 +17,28 @@ router.get('/admin/post', (req, res, next) => {
     }).catch(next);
 });
 
-router.get("/admin/post/count", (req, res, next)=> {
+router.get("/admin/post/count", requireAuth, (req, res, next)=> {
     const {query={}} = req;
     adminService.count({model, query}).then(count=>res.json({count})).catch(next);
 });
 
-router.get('/admin/post/:_id', (req, res, next) => {
+router.get('/admin/post/:_id', requireAuth, (req, res, next) => {
     const {_id} = req.params;
     adminService.find_one({model, _id, empty_error}).then(obj=>res.json(obj)).catch(next);
 });
 
-router.delete('/admin/post/:_id', (req, res, next) => {
+router.delete('/admin/post/:_id', requireAuth, (req, res, next) => {
     const {_id} = req.params;
     adminService.remove_one({model, _id, empty_error}).then(e => res.json(e) ).catch(next);
 });
 
-router.put('/admin/post/:_id', (req, res, next) => {
+router.put('/admin/post/:_id', requireAuth, (req, res, next) => {
     const {_id} = req.params;
     const obj = req.body;
     adminService.update_one({model, _id, obj, empty_error}).then(e => res.json(e) ).catch(next);
 });
 
-router.post('/admin/post', (req, res, next) => {
+router.post('/admin/post', requireAuth, (req, res, next) => {
     const obj = req.body;
     adminService.add_one({model, obj, empty_error}).then(e => res.json(e) ).catch(next);
 });
